02351ac57937efca86353328b0725c8526f45eb9
EnesBerk <enesberkarslan@gmail.com>
2023-09-20 07:42:17 +0300

    farma - tema karıştırdığımız yer. faq alanı eklendi.

---
+++ WebUI/Controllers/AboutUsController.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Context;
+using Domain.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace WebUI.Controllers;
+
+//[Authorize]
+public class AboutUsController : Controller
+{
+    public IActionResult Faq()
+    {
+        return View();
+    }
+
+}
