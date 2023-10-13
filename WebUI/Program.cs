816a26921fc6194aaaa63510714bf0b94c6d0386
Mustafa Unal <mustafa.unal@rsu.com.tr>
2023-10-13 20:59:14 +0300

    area yöntemi kuruldu

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -65,6 +65,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "MyArea",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
