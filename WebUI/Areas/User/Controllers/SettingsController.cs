b43df31a91a3b87376f36740f5f48f03fadcfe36
EnesBerk <enesberkarslan@gmail.com>
2023-10-16 23:29:24 +0300

    settings

---
+++ WebUI/Areas/User/Controllers/SettingsController.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Context;
+using Domain.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebUI.Extensions;
+using WebUI.Models.ViewModels;
+
+namespace WebUI.Areas.User.Controllers
+{
+    [Area("User")]
+    public class SettingsController : BaseController
+    {
+        private readonly UserDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public SettingsController(
+            UserDbContext context,
+            UserManager<ApplicationUser> userManager
+            )
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Update()
+        {
+            return View();  
+        }
+    }
+}
