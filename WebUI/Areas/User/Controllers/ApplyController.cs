8cc234413df6d2f9afd0c05c1b706ccfb5ff3ba9
EnesBerk <enesberkarslan@gmail.com>
2023-10-16 20:53:12 +0300

    pharmacy - list

---
+++ WebUI/Areas/User/Controllers/ApplyController.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Context;
+using Domain.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebUI.Extensions;
+using WebUI.Models.ViewModels;
+
+namespace WebUI.Areas.User.Controllers
+{
+    [Area("User")]
+    public class ApplyController : BaseController
+    {
+        private readonly UserDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ApplyController(
+            UserDbContext context,
+            UserManager<ApplicationUser> userManager
+            )
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult List()
+        {
+            return View();  
+        }
+    }
+}
