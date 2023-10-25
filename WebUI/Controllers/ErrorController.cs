9e35c949deac995a3c25f88f0dfd14e130e3e840
Mustafa Ünal <mustafaunaldev@gmail.com>
2023-10-25 20:23:08 +0300

    404 ve 500 sayfaları eklendi (temaya uygun bir şekilde)

---
+++ WebUI/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers;
+
+public class ErrorController : Controller
+{
+    public IActionResult ErrorHandler(int statusCode)
+    {
+        if (statusCode == 404)
+            return View("PageNotFound");
+        if (statusCode == 500)
+            return View("PageBadRequest");
+        return View();
+    }
+}
