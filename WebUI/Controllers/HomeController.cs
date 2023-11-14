86bdb88f775262c181b5c3da9e0792c0bd6bf39e
Mustafa Ünal <mustafaunaldev@gmail.com>
2023-11-14 11:04:01 +0300

    önce öne çıkan ilanlar sonra da hepsi geliyor

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -17,34 +17,60 @@ public class HomeController : Controller
     }
 
     public IActionResult Index()
-    {
-        var boostedSql = _context.Adverts
-            .Include(x => x.ApplicationUser)
-            .Where(x => x.IsBoosted == true)
-            .AsQueryable();
-        HomeVM vm = new()
+	{
+		var defaultSql = _context.Adverts
+			.Include(x => x.ApplicationUser)
+			.AsQueryable();
+		var boostedSql = _context.Adverts
+			.Include(x => x.ApplicationUser)
+			.Where(x => x.IsBoosted == true)
+			.AsQueryable();
+		HomeVM vm = new()
         {
             Last3Advert = _context.Adverts
             .Include(x => x.ApplicationUser)
             .OrderByDescending(x => x.CreateDate)
             .Take(3)
             .ToList(),
-            BoostedAdvertsTechnician = boostedSql
-            .Where(x => x.Type == Domain.Model.Enum.AdvertType.TECHNICIAN).ToList(),
-            BoostedAdvertsDermocosmetic = boostedSql
-            .Where(x => x.Type == Domain.Model.Enum.AdvertType.TECHNICIAN).ToList(),
-            BoostedAdvertsIntern = boostedSql
-            .Where(x => x.Type == Domain.Model.Enum.AdvertType.INTERN).ToList(),
-            BoostedAdvertsLicense = boostedSql
-            .Where(x => x.Type == Domain.Model.Enum.AdvertType.LICENSE).ToList(),
-            BoostedAdvertsOther = boostedSql
-            .Where(x => x.Type == Domain.Model.Enum.AdvertType.OTHER).ToList(),
+            BoostedAdvertsTechnician = new(),
+            BoostedAdvertsDermocosmetic = new(),
+            BoostedAdvertsIntern = new(),
+            BoostedAdvertsLicense = new(),
+            BoostedAdvertsOther = new(),
             PackagePrice1 = _context.AdvertCategories.Where(x => x.Id == 1).Select(x => x.Price).FirstOrDefault(),
             PackagePrice2 = _context.AdvertCategories.Where(x => x.Id == 2).Select(x => x.Price).FirstOrDefault(),
             PackagePrice3 = _context.AdvertCategories.Where(x => x.Id == 3).Select(x => x.Price).FirstOrDefault(),
         };
+		vm.BoostedAdvertsTechnician.AddRange(boostedSql
+			.Where(x => x.Type == Domain.Model.Enum.AdvertType.TECHNICIAN).ToList());
+		vm.BoostedAdvertsTechnician.AddRange(defaultSql
+			.Where(x => x.Type == Domain.Model.Enum.AdvertType.TECHNICIAN).ToList());
 
-        return View(vm);
+
+		vm.BoostedAdvertsDermocosmetic.AddRange(boostedSql
+			.Where(x => x.Type == Domain.Model.Enum.AdvertType.TECHNICIAN).ToList());
+		vm.BoostedAdvertsDermocosmetic.AddRange(defaultSql
+			.Where(x => x.Type == Domain.Model.Enum.AdvertType.TECHNICIAN).ToList());
+
+
+		vm.BoostedAdvertsIntern.AddRange(boostedSql
+			.Where(x => x.Type == Domain.Model.Enum.AdvertType.INTERN).ToList());
+		vm.BoostedAdvertsIntern.AddRange(defaultSql
+			.Where(x => x.Type == Domain.Model.Enum.AdvertType.INTERN).ToList());
+
+
+		vm.BoostedAdvertsLicense.AddRange(boostedSql
+			.Where(x => x.Type == Domain.Model.Enum.AdvertType.LICENSE).ToList());
+		vm.BoostedAdvertsLicense.AddRange(defaultSql
+			.Where(x => x.Type == Domain.Model.Enum.AdvertType.LICENSE).ToList());
+
+
+		vm.BoostedAdvertsOther.AddRange(boostedSql
+			.Where(x => x.Type == Domain.Model.Enum.AdvertType.OTHER).ToList());
+		vm.BoostedAdvertsOther.AddRange(defaultSql
+			.Where(x => x.Type == Domain.Model.Enum.AdvertType.OTHER).ToList());
+
+		return View(vm);
     }
     public IActionResult Faq()
     {
