a54556d3ee81a82249bbec7709f5de6801866266
Mustafa Unal <mustafa.unal@rsu.com.tr>
2023-11-28 15:26:56 +0300

    migrations

---
+++ DataAccess/Migrations/20231128122632_prod_4.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class prod_4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Resumes");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Resumes");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Resumes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Resumes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Resumes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Resumes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
